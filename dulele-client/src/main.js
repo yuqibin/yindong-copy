a7bb932ebdb16263036540ae2bb8bb536f44d7e7
yuqb <yuqb@133.cn>
2020-08-12 17:40:50 +0800

    nothing

--- dulele-client/src/main.js
+++ dulele-client/src/main.js
@@ -3,34 +3,40 @@ import App from './App.vue'
 import router from './router'
 // import './registerServiceWorker'
 import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Button,
+  Slider,
+  Popover,
+  Backtop,
+  Input,
+  Icon
+} from 'element-ui';
 // import {
-//   Button,
-//   Slider,
-//   Popover,
-//   Backtop,
+//   Button
+// } from 'element-ui/lib/button';
+// import {
+//   Slider
+// } from 'element-ui/lib/slider';
+// import {
+//   Popover
+// } from 'element-ui/lib/popover';
+// import {
+//   Backtop
+// } from 'element-ui/lib/backtop';
+// import {
 //   Input
-// } from 'element-ui';
-import {
-  Button
-} from 'element-ui/lib/button';
-import {
-  Slider
-} from 'element-ui/lib/slider';
-import {
-  Popover
-} from 'element-ui/lib/popover';
-import {
-  Backtop
-} from 'element-ui/lib/backtop';
-import {
-  Input
-} from 'element-ui/lib/input';
+// } from 'element-ui/lib/input';
+// import {
+//   Icon
+// } from 'element-ui/lib/icon';
 
 Vue.component(Backtop.name, Backtop);
 Vue.component(Button.name, Button);
 Vue.component(Slider.name, Slider);
 Vue.component(Popover.name, Popover);
 Vue.component(Input.name, Input);
+Vue.component(Icon.name, Icon);
+// Vue.use(Button, Backtop, Slider, Popover, Input, Icon)
 // import ElementUI from 'element-ui'
 // Vue.use(ElementUI)
 
